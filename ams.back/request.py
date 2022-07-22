19961e3356dcf94c21585dda8ff06db3d0054da9
Joseph Ma <jlsajfj@hotmail.com>
2022-07-22 08:07:25 -0400

    hiding api url in api_url.py

--- ams.back/request.py
+++ ams.back/request.py
@@ -3,15 +3,14 @@ honestly this isn't really even a backend- its simply a wrapper for another api
 """
 
 import requests
-
-url = "https://bdaas.americanexpress.com/api/servicing/v1/maps"
+import api_url
 
 querystring = {"recent_active_indicator":"false","lat_lng":"40.7579747,-73.9855426","page_size":"100"}
 
 payload = ""
 headers = {}
 
-response = requests.request("GET", url, data=payload, headers=headers, params=querystring)
+response = requests.request("GET", api_url.url, data=payload, headers=headers, params=querystring)
 
 # print(response.text)
 
