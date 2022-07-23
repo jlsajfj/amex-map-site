3cda4c5a28952e1a186679ae794a933c9e8c1d76
Joseph Ma <jlsajfj@hotmail.com>
2022-07-23 14:53:30 -0400

    Add country code processing
    by default country code is US, which does not work if the lat/lng
    searched is in a different country. GeoName is added to process lat/lng
    data into a country code before calling AMEX api

--- ams.back/request.py
+++ ams.back/request.py
@@ -14,21 +14,46 @@ def home():
 
 @app.route('/', methods=['POST'])
 def main_api():
+    """
+    main_api processes a given post, requiring either lat & lng or a single lat_lng in the headers
+    """
     if app.debug: print("recieved request")
+    
     f_headers = flask.request.headers
     # if app.debug: print(f_headers)
     # if app.debug: print('lat_lng' in f_headers)
     
-    if not 'lat_lng' in f_headers:
+    if (not 'lat' in f_headers or not 'lng' in f_headers) and not 'lat_lng' in f_headers:
         if app.debug: print('lat/lng missing from request')
         return 'missing lat/lng', 400
     
-    lat_lng = flask.request.headers['lat_lng']
-    if app.debug: print("lat, long:", lat_lng)
+    lat_lng, lat, lng = None, None, None
     
-    query_string = {"recent_active_indicator": "false", "lat_lng": lat_lng, "page_size": "100", "country_code": "CA"}
+    if 'lat_lng' in f_headers:# priority given to single lat_lng input
+        lat_lng = f_headers['lat_lng']
+        in_lat, in_lng = map(float, lat_lng.split(','))
+        # lat_lng = str(in_lat) + "," + str(in_lng)# interesting way to remove whitespace
+    else:
+        in_lat, in_lng = f_headers['lat'], f_headers['lng']
+        # lat_lng = str(in_lat) + "," + str(in_lng)
+    lat_lng = str(in_lat) + "," + str(in_lng)
+    if app.debug: print("lat, long:", in_lat, in_lng)
+    
+    if app.debug: print("fetching country code")
+    geo_query = {"lat": in_lat, "lng": in_lng}
+    geo_resp = requests.request("GET", api_url.geoname, data = "", headers = {}, params = geo_query)
+    geo_data = geo_resp.json()
+    
+    if 'status' in geo_data or geo_resp.status_code != 200:
+        return 'geographical data could not be fetched', 500
+    country_code = geo_data['countryCode']
+    print('country_code:', country_code)
+    
+    query_string = {"recent_active_indicator": "false", "lat_lng": lat_lng, "page_size": "100", "country_code": country_code}
     query_response = requests.request("GET", api_url.amex, data = "", headers = {}, params = query_string)
     if app.debug:
+        print(query_response.request)
+        print(query_response.request.path_url)
         print(query_response)
         print(query_response.status_code)
         # print(query_response.text)
